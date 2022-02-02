9bec0977004cbbd22bfcb1b9c9c20dfe2dfa0e7f
TheProgrammerDude <shas19980805@gmail.com>
2022-02-03 00:35:42 +0530

    updated more button, and more gifs

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,15 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   randomGif: any = {};
+  next: Array<number> = [0, 50, 100, 150, 200, 250, 300, 350, 400];
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
+    this.fetchGif();
+  }
+
+  fetchGif() {
     this.http
       .get(
-        'https://g.tenor.com/v1/search?tag=hugs&locale=en&key=LIVDSRZULELA&limit=200'
+        `https://g.tenor.com/v1/search?tag=hugs&locale=en&key=LIVDSRZULELA&limit=200&next=${
+          this.next[Math.floor(Math.random() * this.next.length)]
+        }`
       )
       .subscribe((data: any) => {
+        // console.log(data);
         var randomGifIndex = Math.floor(Math.random() * data.results.length);
 
         this.randomGif = data.results[randomGifIndex];
